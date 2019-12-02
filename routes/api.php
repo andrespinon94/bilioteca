51c74a2cef810c8509756074f383b6a210c00096
andres <andres_constantino_pinon_apps1ma1819@cev.com>
2019-12-02 14:35:44 +0100

    correciones de rutas

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) 
+{
     return $request->user();
 });
 
-Route::GET('show','book_controller@show');
-
 Route::apiResource('users','user_controller');
 Route::POST('userStore','user_controller@userStore');
-Route::GET('show','user_controller@show');
 Route::POST('login','user_controller@login');
 
-
-Route::group(['middleware'=>['auth']], function(){
-Route::apiResource('books','book_controller');
-Route::POST('store','book_controller@store');
+Route::group(['middleware'=>['auth']], function()
+{
+    Route::apiResource('books','book_controller');
+    Route::GET('list_books','book_controller@list_books');
+    Route::POST('store','book_controller@store');
+    Route::GET('show','user_controller@show');
 });
